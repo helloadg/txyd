83dfd95058ada482c39280bc2ee714282d07f21c
txyd <txyd@txyd.cn>
2017-04-14 19:09:21 +0800

    mysql 事务+原理

---
+++ txyd-mybatis/src/test/java/com/txyd/test/period/PeriodTest.java
@@ -0,0 +1,22 @@
+package com.txyd.test.period;
+
+import com.txyd.mybatis.service.BillPeriodService;
+import com.txyd.test.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Administrator on 2016/6/17.
+ */
+public class PeriodTest extends BaseTest {
+    @Autowired
+    private BillPeriodService billPeriodService;
+
+
+
+    @Test
+    public void test(){
+    
+    }
+
+}
